975f8619ef623719883f2fa95e2f2a03ae2102ee
Jean-Michel Claus <jmc@edx.org>
2014-12-03 15:29:04 +0100

    Addressed third round of comments.

--- lms/static/js/edxnotes/views/toggle_notes_factory.js
+++ lms/static/js/edxnotes/views/toggle_notes_factory.js
@@ -1,47 +1,43 @@
 ;(function (define, undefined) {
     'use strict';
-    define([
-        'jquery', 'underscore', 'js/edxnotes/views/edxnotes_visibility_decorator'
-    ], function($, _, EdxnotesVisibilityDecorator) {
-        return function (visibility, visibilityUrl) {
-            var checkbox = $('p.action-inline > a.action-toggle-notes'),
-                checkboxIcon = checkbox.children('i.checkbox-icon'),
-                toggleCheckbox, toggleAnnotator, sendRequest;
+define([
+    'jquery', 'underscore', 'js/edxnotes/views/edxnotes_visibility_decorator'
+], function($, _, EdxnotesVisibilityDecorator) {
+    return function (visibility, visibilityUrl) {
+        var checkbox = $('p.action-inline > a.action-toggle-notes'),
+            checkboxIcon = checkbox.children('i.checkbox-icon'),
+            toggleNotes, sendRequest;
 
-            toggleCheckbox = function () {
-                checkboxIcon.removeClass('icon-check icon-check-empty');
-                checkboxIcon.addClass(visibility ? 'icon-check' : 'icon-check-empty');
-            };
-
-            toggleAnnotator = function () {
-                toggleCheckbox();
-                if (visibility) {
-                    $('.edx-notes-wrapper').each(function () {
-                        EdxnotesVisibilityDecorator.enableNote(this)
-                    });
-                } else {
-                    EdxnotesVisibilityDecorator.disableNotes();
-                }
-            };
-
-            sendRequest = function () {
-                return $.ajax({
-                    type: 'PUT',
-                    url: window.location.origin + visibilityUrl,
-                    dataType: 'json',
-                    data: JSON.stringify({'visibility': visibility}),
-                    error: function(response) {
-                        console.log(JSON.parse(response.responseText));
-                    }
+        toggleNotes = function () {
+            if (visibility) {
+                $('.edx-notes-wrapper').each(function () {
+                    EdxnotesVisibilityDecorator.enableNote(this)
                 });
-            };
+                checkboxIcon.removeClass('icon-check-empty').addClass('icon-check');
+            } else {
+                EdxnotesVisibilityDecorator.disableNotes();
+                checkboxIcon.removeClass('icon-check').addClass('icon-check-empty');
+            }
+        };
 
-            checkbox.on('click', function (event) {
-                visibility = !visibility;
-                toggleAnnotator();
-                sendRequest();
-                event.preventDefault();
+        sendRequest = function () {
+            return $.ajax({
+                type: 'PUT',
+                url: visibilityUrl,
+                dataType: 'json',
+                data: JSON.stringify({'visibility': visibility}),
+                error: function(response) {
+                    console.log(JSON.parse(response.responseText));
+                }
             });
         };
-    });
+
+        checkbox.on('click', function (event) {
+            visibility = !visibility;
+            toggleNotes();
+            sendRequest();
+            event.preventDefault();
+        });
+    };
+});
 }).call(this, define || RequireJS.define);
